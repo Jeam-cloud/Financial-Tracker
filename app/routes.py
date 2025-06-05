343e6e293d837f044626834f833830616eac523b
Jeam-cloud <rcgalut5@gmail.com>
2025-06-05 00:35:43 -0700

    added database
    linked login and signup to database

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import app, db
 from flask import render_template, flash, url_for, redirect
 from app.forms import LoginForm, SignupForm
+from app.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
 
 @app.route("/")
 
@@ -15,8 +17,15 @@ def login():
     form = LoginForm()
 
     if form.validate_on_submit():
-        flash("user has logged in")
-        return redirect(url_for("index"))
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user and check_password_hash(user.password_hashed, form.password.data):
+
+            flash("user has logged in")
+            return redirect(url_for("index"))
+        else:
+            flash("invalid credentials or user does not exist")
+            return redirect(url_for("login"))
     return render_template("login.html", title="Login", form=form)
 
 @app.route("/signup", methods=["GET", "POST"])
@@ -24,6 +33,20 @@ def signup():
     sform = SignupForm()
 
     if sform.validate_on_submit():
-        flash("user has been added to the database")
-        return redirect(url_for("login"))
+        user = User.query.filter_by(username=sform.username.data).first()
+        if user:
+            flash("user already exists!")
+            return redirect(url_for("login"))
+        else:
+            hashed_password = generate_password_hash(sform.password.data)
+            new_user = User(
+                username=sform.username.data, 
+                password=sform.password.data,
+                password_hashed=hashed_password )
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            flash("user has been added to the database")
+            return redirect(url_for("login"))
     return render_template("signup.html", title="Sign-up", sform=sform)
